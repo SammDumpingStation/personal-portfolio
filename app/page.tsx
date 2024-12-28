58060a9d6309ea18b3b6443e8b450aab88bfde64
Samm Caagbay <sammcaag@gmail.com>
2024-12-28 23:13:58 +0800

    👍 Adding custom grey colors  and adding pages also

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <section>HI</section>;
+  return (
+    <h1 className="font-bold text-[59px] text-center">
+      <span className="text-ugray-500">Hello! </span>
+      Samm Caagbay, Full Stack Web Developer, Based in Philippines
+    </h1>
+  );
 }
