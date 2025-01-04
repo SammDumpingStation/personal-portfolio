51ee44d9aae60a6c9f89771beb385ba414db7c0e
Samm Caagbay <sammcaag@gmail.com>
2025-01-04 16:54:53 +0800

    👍 Adding Small Effects in the Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,33 @@
+"use client";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Card } from "./ui/card";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
+type NavLinkProps = { href: string; title: string };
 const Navbar = () => {
+  const pathname = usePathname();
+  const NavLink = ({ href, title }: NavLinkProps) => (
+    <Link
+      className={clsx(
+        "hover:text-base hover:font-bold [transition-timing-function:cubic-bezier(0.25,0.1,0.25,1)] transition-all duration-300",
+        pathname === href
+          ? "text-ugray-600 dark:text-ugray-200 font-bold text-base"
+          : "text-ugray-400 dark:text-ugray-300 font-medium text-sm"
+      )}
+      href={href}
+    >
+      {title}
+    </Link>
+  );
   return (
     <nav className="fixed top-[50px] z-50 left-1/2 -translate-x-1/2">
-      <Card className="flex items-center gap-12 text-ugray-800 dark:text-ugray-300 font-semibold text-sm px-12 h-[56px]  rounded-full">
-        <Link href={"/"}>Home</Link>
-        <Link href={"/portfolio"}>Portfolio</Link>
-        <Link href={"/about-me"}>About Me</Link>
-        <Link href={"/contact"}>Contact</Link>
+      <Card className="flex items-center w-[455px] px-5 justify-around h-[56px]  rounded-full">
+        <NavLink href="/" title="Home" />
+        <NavLink href="/portfolio" title="Portfolio" />
+        <NavLink href="/about" title="About" />
+        <NavLink href="/contact" title="Contact" />
       </Card>
     </nav>
   );
