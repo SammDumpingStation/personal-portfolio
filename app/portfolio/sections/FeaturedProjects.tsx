cb647022b439e4976a68a72d74509dd959c1e07d
Samm Caagbay <sammcaag@gmail.com>
2025-01-05 21:35:58 +0800

    👍 &apos; fix

--- app/portfolio/sections/FeaturedProjects.tsx
+++ app/portfolio/sections/FeaturedProjects.tsx
@@ -8,8 +8,8 @@ const FeaturedProjects = () => {
         Featured <span className="text-ugray-500">Projects</span>
       </h3>
       <p className="my-journey-p">
-        Explore the diverse range of digital products I've designed, from MVPs
-        to fully developed apps and websites.
+        Explore the diverse range of digital products I&apos;ve designed, from
+        MVPs to fully developed apps and websites.
       </p>
       <div className="min-h-[660px] px-4 flex  gap-6 w-full max-w-[1018px] max-lg:flex-col max-lg:items-center flex-wrap">
         <PortfolioCard />
